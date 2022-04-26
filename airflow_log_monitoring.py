a6f2fe741601ca937075a5a9e56d32dbb220a169
caseyfrost <caseyfrost13@gmail.com>
2022-04-26 06:57:10 -0700

    replaced the counter with the len function to shorten the script

--- airflow_log_monitoring.py
+++ airflow_log_monitoring.py
@@ -1,4 +1,12 @@
+from datetime import timedelta
 from pathlib import Path
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+# The folder where airflow should store its log files
+# This path must be absolute
+# marketvol log directory
+log_dir = r'/Users/caseyfrost/airflow/logs/marketvol'
 
 
 def analyze_file(file):
@@ -12,31 +20,37 @@ def analyze_file(file):
     return error_count, error_messages
 
 
-def print_results(result):
-    pass
+def main(logs):
+    file_list = Path(logs).rglob('*.log')
+    errors = []
+    for file in file_list:
+        count, cur_list = analyze_file(file)
+        if len(cur_list) > 0:
+            for error in cur_list:
+                errors.append(error.strip('\n'))
+    print(f'Total errors: {len(errors)}')
+    for error in errors:
+        print(error)
 
 
-# The folder where airflow should store its log files
-# This path must be absolute
-base_log_folder = r'/Users/caseyfrost/airflow/logs'
+default_args = {
+    'owner': 'cfrost',
+    'start_date': '2022-4-22',
+    'end_date': '2022-5-1',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
 
-# marketvol log directory
-log_dir = r'/Users/caseyfrost/airflow/logs/marketvol'
+dag = DAG(
+    dag_id='error_logs',
+    default_args=default_args,
+    description='Sum count of error logs and print error messages',
+    schedule_interval='@once',
+)
 
-file_list = Path(log_dir).rglob('*.log')
-for file in file_list:
-    print(file)
-
-errors = []
-total_errors = 0
-
-for file in file_list:
-    count, cur_list = analyze_file(file)
-    total_errors += count
-    if len(cur_list) > 0:
-        for error in cur_list:
-            errors.append(error.strip('\n'))
-
-print(total_errors)
-for error in errors:
-    print(error)
+t0 = PythonOperator(
+    task_id='error_report',
+    dag=dag,
+    python_callable=main,
+    op_kwargs={'logs': log_dir}
+)
